2c0362fc1d4e3463e62e4e2e84c29d6922e85f52
RASHIKATYAGI99 <rashikatyagi99@gmail.com>
2025-07-08 22:11:35 +0530

    Day 1: Added banner image and homepage layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import banner from "../assets/banner.png"; 
 
 const Home = () => {
   return (
     <div>
+      <img src={banner} alt="Banner" style={{ width: "100%", maxHeight: "300px", objectFit: "cover" }} />
       <h2>Welcome to EduJobMatch</h2>
       <p>Check your eligibility for exams and apply directly!</p>
     </div>
