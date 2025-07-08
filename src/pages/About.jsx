6e324a5f5b2d10bb0585500d30db3639ec39f6e3
RASHIKATYAGI99 <rashikatyagi99@gmail.com>
2025-07-08 21:38:20 +0530

    Day 1: Added Home and About pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h2>About EduJobMatch</h2>
+      <p>This platform helps users discover exams they’re eligible for based on their education.</p>
+    </div>
+  );
+};
+
+export default About;
