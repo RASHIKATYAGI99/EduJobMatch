ccb9823d74c050b8850cc101d422966d23a837e9
RASHIKATYAGI99 <rashikatyagi99@gmail.com>
2025-07-13 18:53:21 +0530

    Update Navbar styling and active link behavior

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,11 @@ const Navbar = () => {
     <nav className="navbar">
       <h2 style={{ color: "#fff" }}>EduJobMatch</h2>
       <div>
-        <NavLink to="/" end className="nav" activeClassName="active">Home</NavLink>
+       <NavLink to="/" end className="nav" activeClassName="active">Home</NavLink>
 <NavLink to="/about" className="nav" activeClassName="active">About</NavLink>
 <NavLink to="/eligibility" className="nav" activeClassName="active">Eligibility</NavLink>
 <NavLink to="/contact" className="nav" activeClassName="active">Contact</NavLink>
+
 
       </div>
     </nav>
