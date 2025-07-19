d4f79df9b126e4d6d1fdb8cafa373defc5cc02df
RASHIKATYAGI99 <rashikatyagi99@gmail.com>
2025-07-19 16:10:09 +0530

    Create Job model schema for MongoDB

---
+++ server/models/Job.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const jobSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  qualification: {
+    type: String,
+    required: true,
+  },
+  applyLink: {
+    type: String,
+    required: true,
+  },
+  postedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Job', jobSchema);
